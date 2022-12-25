431a07b32463703ba814858d7ab3d6efe5e7116c
Christian Wolf <github@christianwolf.email>
2022-12-25 10:40:05 +0100

    Simplify app structure and fix code styling
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 /**
- * SPDX-FileCopyrightText: 2018 John Molakvoæ <skjnldsv@protonmail.com>
+ * SPDX-FileCopyrightText: 2022 Christian Wolf <github@christianwolf.email>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-// import { generateFilePath } from '@nextcloud/router'
-
-// import Vue from 'vue'
-// import App from './App'
-
-// // eslint-disable-next-line
-// __webpack_public_path__ = generateFilePath(appName, '', 'js/')
-
-// Vue.mixin({ methods: { t, n } })
-
-// export default new Vue({
-// 	el: '#content',
-// 	render: h => h(App),
-// })
+// This is only a stub as the original webpack config requires a main.js
--- src/snow.js
+++ src/snow.js
@@ -14,147 +14,160 @@
 
 import { loadState } from '@nextcloud/initial-state'
 
-(function (){
-    // const val = loadState('myapp', 'user_preference')
-    
-    // Provide a fallback value to return when the state is not found
-    const defaultSettings = {
-        numFlakes: 35,
-        color: ["#DDD", "#EEE"],
-        speed: 0.75,
-        size: [5, 20],
-        refresh: 15,
-        lrMultiplicator: 10,
-        lrDivider: 20
-    }
-    const settings = loadState('snowflakestheme', 'settings', defaultSettings);
-    window.console.debug('Using setting for the snowflakes', settings);
+(function() {
+	// const val = loadState('myapp', 'user_preference')
 
-    // Amount of Snowflakes
-    // Recommended: 35
-    const snowMax = settings.numFlakes;
+	// Provide a fallback value to return when the state is not found
+	const defaultSettings = {
+		numFlakes: 35,
+		color: ['#DDD', '#EEE'],
+		speed: 0.75,
+		size: [5, 20],
+		refresh: 15,
+		lrMultiplicator: 10,
+		lrDivider: 20,
+	}
+	const settings = loadState('snowflakestheme', 'settings', defaultSettings)
+	window.console.debug('Using setting for the snowflakes', settings)
 
-    // Snowflake Colours
-    const snowColor = settings.color;
+	// Amount of Snowflakes
+	// Recommended: 35
+	const snowMax = settings.numFlakes
 
-    // Snow Entity
-    const snowEntity = "&#x2022;";
+	// Snowflake Colours
+	const snowColor = settings.color
 
-    // Falling Velocity
-    // Recommended: 0.75
-    const snowSpeed = settings.speed;
+	// Snow Entity
+	const snowEntity = '&#x2022;'
 
-    // Minimum Flake Size
-    // Recommended: 8
-    const snowMinSize = settings.size[0];
+	// Falling Velocity
+	// Recommended: 0.75
+	const snowSpeed = settings.speed
 
-    // Maximum Flake Size
-    // Recommended: 24
-    const snowMaxSize = settings.size[1];
+	// Minimum Flake Size
+	// Recommended: 8
+	const snowMinSize = settings.size[0]
 
-    // Refresh Rate (in milliseconds)
-    // Recommended: 50
-    const snowRefresh = settings.refresh;
+	// Maximum Flake Size
+	// Recommended: 24
+	const snowMaxSize = settings.size[1]
 
-    // Additional Styles
-    const snowStyles = "cursor: default; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; -o-user-select: none; user-select: none;";
+	// Refresh Rate (in milliseconds)
+	// Recommended: 50
+	const snowRefresh = settings.refresh
 
-    /*
+	// Additional Styles
+	const snowStyles = 'cursor: default; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; -o-user-select: none; user-select: none;'
+
+	/*
     // Experimental Configuration
     // ----------------------------------------
     // Do only modify this, if you know what you do. Otherwise keep in mind, that it can break the functionallity.
     */
 
-    // The Z-Index where the snowflakes are leveled. Higher values is more in the front.
-    // Increase this value if your snowflakes are not (or only partly) visible.
-    // Recommended: 1000
-    const snowZIndex = 100100;
+	// The Z-Index where the snowflakes are leveled. Higher values is more in the front.
+	// Increase this value if your snowflakes are not (or only partly) visible.
+	// Recommended: 1000
+	const snowZIndex = 100100
 
-    // How much the snowflakes should move to left and back to right.
-    // Higher Value = More
-    // Recommended: 15
-    const snowLeftRightMultiplicator = settings.lrMultiplicator;
+	// How much the snowflakes should move to left and back to right.
+	// Higher Value = More
+	// Recommended: 15
+	const snowLeftRightMultiplicator = settings.lrMultiplicator
 
-    // How fast the snowflakes should move to left and back to right.
-    // Higher Value = Slower
-    // Recommended: 10
-    const snowLeftRightSpeedDivider = settings.lrDivider;
+	// How fast the snowflakes should move to left and back to right.
+	// Higher Value = Slower
+	// Recommended: 10
+	const snowLeftRightSpeedDivider = settings.lrDivider
 
-    // The tolerance where the showflakes get despawned.
-    // Recommended: 2
-    const marginBottmTolerance = 5;
+	// The tolerance where the showflakes get despawned.
+	// Recommended: 2
+	const marginBottmTolerance = 5
 
-    /*
+	/*
     // End of Configuration
     // ----------------------------------------
     // Do not modify the code below this line
     */
 
-    var snow = [],
-        pos = [],
-        coords = [],
-        lefr = [],
-        marginBottom,
-        marginRight;
+	const snow = []
+	const pos = []
+	const coords = []
+	const lefr = []
+	let marginBottom
+	let marginRight
 
-    function randomise(range) {
-        return Math.floor(range * Math.random());
-    }
+	/**
+	 *
+	 * @param range
+	 */
+	function randomise(range) {
+		return Math.floor(range * Math.random())
+	}
 
-    function initSnow() {
-        var snowSize = snowMaxSize - snowMinSize;
-        resize();
+	/**
+	 *
+	 */
+	function initSnow() {
+		const snowSize = snowMaxSize - snowMinSize
+		resize()
 
-        for (let i = 0; i <= snowMax; i++) {
-            coords[i] = 0;
-            lefr[i] = Math.random() * snowLeftRightMultiplicator;
-            pos[i] = 0.03 + Math.random() / snowLeftRightSpeedDivider;
-            snow[i] = document.getElementById("flake" + i);
-            snow[i].style.fontFamily = "inherit";
-            snow[i].size = randomise(snowSize) + snowMinSize;
-            snow[i].style.fontSize = snow[i].size + "px";
-            snow[i].style.color = snowColor[randomise(snowColor.length)];
-            snow[i].style.zIndex = snowZIndex;
-            snow[i].sink = snowSpeed * snow[i].size / 5;
-            snow[i].posX = randomise(marginRight - snow[i].size);
-            snow[i].posY = randomise(2 * marginBottom - marginBottom - 2 * snow[i].size);
-            snow[i].style.left = snow[i].posX + "px";
-            snow[i].style.top = snow[i].posY + "px";
-        }
+		for (let i = 0; i <= snowMax; i++) {
+			coords[i] = 0
+			lefr[i] = Math.random() * snowLeftRightMultiplicator
+			pos[i] = 0.03 + Math.random() / snowLeftRightSpeedDivider
+			snow[i] = document.getElementById('flake' + i)
+			snow[i].style.fontFamily = 'inherit'
+			snow[i].size = randomise(snowSize) + snowMinSize
+			snow[i].style.fontSize = snow[i].size + 'px'
+			snow[i].style.color = snowColor[randomise(snowColor.length)]
+			snow[i].style.zIndex = snowZIndex
+			snow[i].sink = snowSpeed * snow[i].size / 5
+			snow[i].posX = randomise(marginRight - snow[i].size)
+			snow[i].posY = randomise(2 * marginBottom - marginBottom - 2 * snow[i].size)
+			snow[i].style.left = snow[i].posX + 'px'
+			snow[i].style.top = snow[i].posY + 'px'
+		}
 
-        moveSnow();
-    }
+		moveSnow()
+	}
 
-    function resize() {
-        marginBottom = document.body.scrollHeight - 5;
-        marginRight = document.body.clientWidth - 15;
-    }
+	/**
+	 *
+	 */
+	function resize() {
+		marginBottom = document.body.scrollHeight - 5
+		marginRight = document.body.clientWidth - 15
+	}
 
-    function moveSnow() {
-        for (let i = 0; i <= snowMax; i++) {
-            coords[i] += pos[i];
-            snow[i].posY += snow[i].sink;
-            snow[i].style.left = snow[i].posX + lefr[i] * Math.sin(coords[i]) + "px";
-            snow[i].style.top = snow[i].posY + "px";
+	/**
+	 *
+	 */
+	function moveSnow() {
+		for (let i = 0; i <= snowMax; i++) {
+			coords[i] += pos[i]
+			snow[i].posY += snow[i].sink
+			snow[i].style.left = snow[i].posX + lefr[i] * Math.sin(coords[i]) + 'px'
+			snow[i].style.top = snow[i].posY + 'px'
 
-            if (snow[i].posY >= marginBottom - marginBottmTolerance * snow[i].size || parseInt(snow[i].style.left) > (marginRight - 3 * lefr[i])) {
-                snow[i].posX = randomise(marginRight - snow[i].size);
-                snow[i].posY = 0;
-            }
-        }
+			if (snow[i].posY >= marginBottom - marginBottmTolerance * snow[i].size || parseInt(snow[i].style.left) > (marginRight - 3 * lefr[i])) {
+				snow[i].posX = randomise(marginRight - snow[i].size)
+				snow[i].posY = 0
+			}
+		}
 
-        window.setTimeout(moveSnow, snowRefresh);
-    }
+		window.setTimeout(moveSnow, snowRefresh)
+	}
 
-    for (let i = 0; i <= snowMax; i++) {
-        var element = document.createElement("span");
-        element.id = "flake" + i;
-        element.style = snowStyles + "position:absolute;top:-" + snowMaxSize;
-        element.innerHTML = snowEntity;
-        document.body.insertBefore(element, document.body.firstChild);
-    }
+	for (let i = 0; i <= snowMax; i++) {
+		const element = document.createElement('span')
+		element.id = 'flake' + i
+		element.style = snowStyles + 'position:absolute;top:-' + snowMaxSize
+		element.innerHTML = snowEntity
+		document.body.insertBefore(element, document.body.firstChild)
+	}
 
-    initSnow();
-    // window.addEventListener('load', initSnow);
-    window.addEventListener('resize', resize);
-})();
+	initSnow()
+	// window.addEventListener('load', initSnow);
+	window.addEventListener('resize', resize)
+})()
