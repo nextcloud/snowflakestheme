6ed3f875b2f8d121925f395e466d14cb20ecbd68
Christian Wolf <github@christianwolf.email>
2024-12-07 16:18:43 +0100

    Fix building script
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- .github/create-signature.sh
+++ .github/create-signature.sh
@@ -4,5 +4,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 composer install --no-dev
+npm run build
 make appstore
 openssl dgst -sha512 -sign ~/.nextcloud/certificates/snowflakestheme/snowflakestheme.key build/artifacts/appstore/snowflakestheme.tar.gz | openssl base64
